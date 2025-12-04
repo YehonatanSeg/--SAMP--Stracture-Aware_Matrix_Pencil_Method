3f8cf58a32d7c7849da89ef911827fbfd57a11bd
YehonatanSeg <63717184+YehonatanSeg@users.noreply.github.com>
2025-12-04 10:57:44 +0200

    Update SAMP algorithm and fig (2)
    Updates correspond to the new SAMP version from 9/25.

--- SAMP/SAMP_Algorithm_and_fig_2.m
+++ SAMP/SAMP_Algorithm_and_fig_2.m
@@ -5,13 +5,16 @@ close all;
 
 % Settings for simulating sum of complex exponentials by Eq (2)
 params.dt = 1/10;
-params.interval_length = 10;
+params.interval_length = 7;
 N = 1 +params.interval_length / params.dt;
-w1 = 2;
-params.true_freqs  = [w1 + 1.5*pi/(params.dt*N), w1 ] ; 
+
+w1 = 2; 
+
+params.true_freqs  = [w1 + 2*pi/(params.dt*N), w1] ; 
 params.true_damps  = [0.0, 0.0];
 params.true_amps   = [1, 1];
 params.true_phases = [0, 0];
+
 params.true_poles = exp(-params.true_damps*params.dt + 1i*params.true_freqs*params.dt);
 SNR = 10;
 
@@ -27,46 +30,82 @@ Y = create_hankel_matrix(params.signal,params.pencil_parameter);
 %% The SAMP Algorithm (Algorithm 1)
 
 %Modes and Eigenvalues Computation by step 1:
-[left_mode, right_mode, lambda_mp] = get_MP_components(Y); % ll_mdl, ll_aic
+[left_modes, right_modes, mp_poles] = get_MP_components(Y); 
 
 % Parameter Estimation by step 2:
-mp_freqs  = imag(log(lambda_mp))/params.dt;
-mp_damps  = real(log(lambda_mp)) /params.dt;
-b_tilde   = (left_mode(1,:).') .* right_mode(:,1); % Efficient amplitude estimation by Eq (43)
+mp_freqs  = imag(log(mp_poles))/params.dt;
+mp_damps  = real(log(mp_poles)) /params.dt;
+b_tilde   = (left_modes(1,:).') .* right_modes(:,1); % Efficient amplitude estimation by Eq (44)
 
 % Model Order Detection by step 3:
-Nfft = 2^(nextpow2(size(left_mode,1)));
-y = fft(left_mode-mean(left_mode,1), Nfft, 1);
-y = abs(y); 
-[max_mode_peaks, ~]  = max(y);
-epsilon = (max_mode_peaks') ;  % Signal detection features by Eq (41)
-freqs = angle(lambda_mp)/params.dt;
-diff_matrix = abs(freqs - freqs.').^2;
-freqs_sum_diff = sum(diff_matrix, 2);
-epsilon = epsilon./freqs_sum_diff;  % Normalized signal detection features by Eq (42)
 
-% Deviding the signal detection features into two distinct groups using
-% k-means algorithm and selecting the signal related indices 
-idx            = kmeans(epsilon,2,'Replicates', 1);
-avg(1)         = mean(epsilon( idx == 1 ));
-avg(2)         = mean(epsilon( idx == 2 ));
-[~,true_group] = max(avg);
-signal_ind     = find(idx == true_group);
-M_tilde        = length(signal_ind); % model order estimation
+%  Normalized similaruty measure by Eq (36)
+P = compute_similarity_measure(left_modes, params);
+
+% Signal detection features by Eq (40)
+epsilon = max(P  , [] , 2 ); 
+
+% Normalized signal detection features by Eq (43)
+diff_matrix = (mp_poles ./ mp_poles.'); 
+damps_sum_diff = sum(abs(diff_matrix).^2 , 1).^2;
+epsilon = epsilon ./ damps_sum_diff(:);
+epsilon = epsilon/max(epsilon);
+
+
+% Feature binary partition into two distinct subsets by
+% defining the signal-related subset as the subset of
+% features satisfying Eq. (46):
+bt_i        =  abs(b_tilde);              
+N_L         = size(left_modes,1);
+A           = @(z) (z .^ (0:N_L-1)).';
+c           = 10*sqrt(N_L)./ vecnorm(A(mp_poles)); 
+ISR         = c(:) ./ bt_i ;
+P_low_bound = ( (1-ISR) ./ (1+ISR) ).^2 ;
+signal_ind  = find( epsilon(:) > P_low_bound(:) );
+
+% model order estimation
+M_tilde     = length(signal_ind); 
 
 % Parameter Selection by step 4:
-est_freqs = mp_freqs(signal_ind);
-est_damps = mp_damps(signal_ind);
-est_amps  = b_tilde(signal_ind);
+est_freqs   = mp_freqs(signal_ind);
+est_damps   = mp_damps(signal_ind);
+est_amps    = b_tilde(signal_ind);
 
 %% Figs
 %Fig. (2) in the paper
-plot_modes_FFT(left_mode, params, signal_ind)
 
+fig = figure();  hold on;
 
-
+all_dist = abs(mp_freqs - params.true_freqs).^2;
+pairs = matchpairs(all_dist,1e16);
+xline(pairs(:,1), 'LineWidth',3,'color','red'); 
+plot(epsilon,'o', 'LineWidth',2); 
+plot(P_low_bound, 'x', 'color','black', 'LineWidth',2);
+xlabel('Mode index','FontSize',14)
+ylabel('$P_i(z_i^*)$', 'Interpreter','latex','FontSize',14)
 
 %% ======================= AUX FUNCTIONS ======================= %%
+function P = compute_similarity_measure(left_modes, params)
+    % Define the Grid of frequencies x dampings
+    N_grid = 2^(nextpow2(size(left_modes,1)));
+    thetaGrid = linspace(-pi, pi, N_grid);
+    damp_grid = linspace(0, 1, N_grid)';
+    lambda_grid = exp(-(damp_grid*params.dt) + 1i*thetaGrid*params.dt);
+    
+    left_modes_n = left_modes./vecnorm(left_modes);
+    N = size(left_modes_n,1);
+    
+    % --- build Vandermonde for *all* grid points ----------------------------
+    zvec   = lambda_grid(:).';                     
+    Agrid  =  zvec.^((0:N-1).') ;
+    denom  = sum(abs(Agrid).^2,1);      
+    
+    % --- numerator for every mode & grid point in one shot ------------------
+    num    = abs( left_modes_n' * Agrid ).^2;        
+    
+    % --- Compute the normalized similarity measure --------------------------
+    P = num ./ denom;
+end
 
 function [left_mode, right_mode, lambda] = get_MP_components(Y)
 
@@ -97,71 +136,4 @@ function [left_mode, right_mode, lambda] = get_MP_components(Y)
         left_mode  = Ur*Sr*Q;
         right_mode = Q \ Vr';       
         
-end
-
-
-function plot_modes_FFT(left_mode, params, true_ind)
-
-Nfft = 2^(3+nextpow2(size(left_mode,1)));
-y = fft(left_mode-mean(left_mode,1),Nfft, 1);
-y = fft(left_mode,Nfft, 1);
-
-y = abs(y);
-global_max = max(y,[],'all');
-y = y/global_max;
-frequencies=(0:Nfft-1)*(2*pi/(params.dt*Nfft));
-
-% re-ordering the indices according to the true-frequencies
-[~, freqs_ind] = max(y(:,true_ind));
-est_freqs = frequencies(freqs_ind);
-dist = abs(est_freqs' - params.true_freqs);
-if sum(diag(dist)) > sum(diag(flip(dist)))
-    true_ind_ord(1) = true_ind(2);
-    true_ind_ord(2) = true_ind(1);
-    true_ind = true_ind_ord;
-end
-
-% These setting are for placing 4 subfigures in one column..
-set(0,'DefaultTextFontName','Times',...
-'DefaultTextFontSize',75,...
-'DefaultAxesFontName','Times',...
-'DefaultAxesFontSize',75,...
-'DefaultLineLineWidth',4.5,...
-'DefaultLineMarkerSize',15);
-
-fig1 = figure(); hold on;
-fig1.WindowState = 'maximized';  
-plot(frequencies,y(:,true_ind(1))); 
-ylim([0,1]);
-ylabel('Coefficients')
-xlabel('$\theta$', 'Interpreter', 'latex')
-plot([params.true_freqs(1) params.true_freqs(1)],[0 1], '-.','color', 'black');
-legend('$|\mathcal{F}(\widetilde{\mathbf{Z}}_L^{1})|$', '$\theta_1$', 'Interpreter', 'latex')
-
-fig2 = figure(); hold on;
-fig2.WindowState = 'maximized';
-plot(frequencies,y(:,true_ind(2))); 
-ylim([0,1]);
-ylabel('Coefficients')
-xlabel('$\theta$', 'Interpreter', 'latex')
-plot([params.true_freqs(2) params.true_freqs(2)],[0 1], '-.','color', 'black');
-legend('$|\mathcal{F}(\widetilde{\mathbf{Z}}_L^{2})|$', '$\theta_2$', 'Interpreter', 'latex')
-
-fig3 = figure();
-fig3.WindowState = 'maximized';
-plot(frequencies,y(:,max(true_ind)+1))
-ylim([0,1]);
-ylabel('Coefficients')
-xlabel('$\theta$', 'Interpreter', 'latex')
-legend('$|\mathcal{F}(\widetilde{\mathbf{Z}}_L^{3})|$', 'Interpreter', 'latex')
-
-fig4 = figure();
-fig4.WindowState = 'maximized';
-plot(frequencies,y(:,20))
-ylim([0,1]);
-ylabel('Coefficients')
-xlabel('$\theta$', 'Interpreter', 'latex')
-legend('$|\mathcal{F}(\widetilde{\mathbf{Z}}_L^{20})|$', 'Interpreter', 'latex')
-
-
 end
